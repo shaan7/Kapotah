abd6ba02eb573619fe0c164d9b9b3787ac7bcb38
Sudhendu Kumar <sudhendu.kumar.roy@gmail.com>
2011-01-06 19:54:16 +0530

    Added messagexmlparser.

---
+++ xml/messagexmlparser.h
@@ -0,0 +1,39 @@
+/*
+    This file is part of the Kapotah project.
+    Copyright (C) 2011 Shantanu Tushar <jhahoneyk@gmail.com>
+    Copyright (C) 2011 Sudhendu Kumar <sudhendu.kumar.roy@gmail.com>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef MESSAGEXMLPARSER_H
+#define MESSAGEXMLPARSER_H
+
+#include "abstractxmlparser.h"
+#include <QString>
+
+class MessageXMLData : public AbstractXMLData
+{
+    public:
+        QString message;
+};
+
+class MessageXMLParser : public AbstractXMLParser
+{
+    public:
+        virtual QString composeXML (AbstractXMLData *data);
+        virtual AbstractXMLData* parseXML (const QString& xml);
+};
+
+#endif // MESSAGEXMLPARSER_H
