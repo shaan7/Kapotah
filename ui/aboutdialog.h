d3ed6b8431167f453a0b7b0e956489c6280a9424
Shantanu Tushar <jhahoneyk@gmail.com>
2011-05-05 21:53:01 +0530

    Add about dialog.

---
+++ ui/aboutdialog.h
@@ -0,0 +1,34 @@
+/*
+    This file is part of the Kapotah project.
+    Copyright (C) 2011 Shantanu Tushar <jhahoneyk@gmail.com>
+    Copyright (C) 2011 Sudhendu Kumar <sudhendu.kumar.roy@gmail.com>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef ABOUTDIALOG_H
+#define ABOUTDIALOG_H
+
+#include "ui_aboutdialog.h"
+
+class AboutDialog : public QDialog {
+    Q_OBJECT
+public:
+    explicit AboutDialog(QWidget *parent = 0);
+
+private:
+    Ui::AboutDialog m_ui;
+};
+
+#endif // ABOUTDIALOG_H
