9073028e23f4888aff7622e376c4a99c9bff6a92
Sudhendu Kumar <sudhendu.kumar.roy@gmail.com>
2011-05-05 22:53:45 +0530

    made about dialog functional. and some factoring done.

--- ui/peerdialog.h
+++ ui/peerdialog.h
@@ -32,6 +32,7 @@ class ChatDialog;
 class SearchDialog;
 class MulticastDialog;
 class TransferDialog;
+class AboutDialog;
 
 class PeerDialog : public QDialog
 {
@@ -47,6 +48,7 @@ class PeerDialog : public QDialog
         TransferDialog *m_transferDialog;
         MulticastDialog *m_openMulticastDialog;
         SearchDialog *m_openSearchDialog;
+        AboutDialog *m_openAboutDialog;
 
         void createTrayIcon();
         void createActions();
@@ -65,8 +67,6 @@ class PeerDialog : public QDialog
 
     private slots:
         void setPeerNameFromUi();
-
-    private slots:
         ChatDialog* createChatDialog (QModelIndex index);
         void showChatDialogOnUserRequest(QModelIndex index);
         void showChatDialogOnIncomingMessage (QString message, QHostAddress peerAddress);
@@ -82,6 +82,7 @@ class PeerDialog : public QDialog
         void initSubnets();
         void askUserForNewAdditionalSubnet();
         void removeSelectedAdditionalSubnet();
+        AboutDialog* showAboutDialog();
 };
 
 #endif // PEERDIALOG_H
